057ba787c8168b60a33996a9cbb31a47432a207c
BluDolphin <github.com.tablet296@passmail.net>
2024-04-15 22:42:21 +0100

    Create YTDownloader.py

---
+++ Code/YTDownloader.py
@@ -0,0 +1,48 @@
+import yt_dlp
+
+def audioOrVideo():
+    print("\nWhich do you want to download:"
+          "\n1) Audio"
+          "\n2) Video")
+    
+    if (temp:=input("Input: ")) == "1":
+        return "bestaudio/best" #Return True for audio
+    elif temp == "2": 
+        return "bestvideo/best" #Return False for video
+    else:
+        print("Invalid Input")
+        audioOrVideo()
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print("\nFinished downloading: ", d['filename'])
+
+def Download():
+    print("\n========= Youtube Downloader ========="
+          "\nPlaylists need to be set to public or unlisted to be downloaded."
+          "\nInput the URL of the playlist or single to download:")
+    usrInput = input("\nURL: ")
+    
+    AorV = audioOrVideo()
+    
+    print("\nStarting download...")
+    
+    ydl_opts = {
+    'noplaylist': False,  # Download just the video, if the URL refers to a video and a playlist.
+    'quiet': True,  # Do not print messages to stdout.
+    'format': AorV,  # Choice of quality.
+    'outtmpl': 'downloads/%(title)s.%(ext)s',  # Name the file the ID of the video
+    'restrictfilenames': True,  # Restrict filenames to only ASCII characters, and avoid "&" and spaces in filenames
+    'nooverwrites': True,  # Prevent overwriting files.
+    'continuedl': True,  # Force resume of partially downloaded files.
+    'progress_hooks': [my_hook],  # Hook function that gets called before the download starts
+    }
+    
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([usrInput])
+    except:
+        print("\nError: Invalid URL")
+        Download()
+
+Download()
